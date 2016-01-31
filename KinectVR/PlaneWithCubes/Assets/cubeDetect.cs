6650cf1b69fb051f09816cf8702b2acdf242cb24
Ankith Uppunda <ankithuppunda@Ankiths-MacBook-Pro.local>
2016-01-30 20:03:01 -0800

    I made the two scripts one to make the cubes spin, and another to get a boolean

---
+++ KinectVR/PlaneWithCubes/Assets/cubeDetect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeDetect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		touch visionDetect = GetComponent<touch>();
+		if (visionDetect.getTouch()) { 
+			this.transform.Rotate (1, 1, 1);
+		}
+	}
+}
